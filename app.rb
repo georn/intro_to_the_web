66e8335b6010da378d5593e2170fe7bb10f05bbd
Guillermo <mrog0@outlook.com>
2017-05-22 17:54:17 +0100

    params

--- app.rb
+++ app.rb
@@ -25,7 +25,13 @@ get "/pizza" do
   "Yes, please."
 end
 
-get "/cat" do
+get "/random-cat" do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get "/named-cat" do
+  p params
+  @name = params[:name]
   erb(:index)
 end
